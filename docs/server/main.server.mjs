64291e51f0ffeca32baf5381260eeaf8aac6e898
AbdullahElalfy <abdullahelalfy.dev@gmail.com>
2025-08-16 14:11:23 +0300

    prod

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-PPA2PWNL.mjs";import"./chunk-354ODLBY.mjs";import"./chunk-SXW42VS4.mjs";import"./chunk-WTLAGS5M.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
